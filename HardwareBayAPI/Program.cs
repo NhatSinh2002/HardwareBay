7f9cd198a62c8a05590755e2619d9a1d6c8b6857
NhatSinh <nhatsinh2002.pt@gmail.com>
2025-01-03 16:41:49 +0700

    commit ConnectionString

--- HardwareBayAPI/Program.cs
+++ HardwareBayAPI/Program.cs
@@ -1,3 +1,6 @@
+﻿using HardwareBayAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Thêm DbContext với Connection String
+builder.Services.AddDbContext<HardwareDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HardWareBayConnectionString")));
+
+
+
 
 var app = builder.Build();
 
